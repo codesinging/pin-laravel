9bfe7821491d7e8f07b2b0c4899b2bcc8cd96717
codesinging <codesinging@gmail.com>
2022-06-12 16:47:22 +0800

    修改用户表结构，增加用户表填充数据

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,36 +3,23 @@
 namespace App\Models;
 
 use App\Support\Model\UserModel;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class User extends UserModel
 {
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
-        'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
-        'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    protected function password(): Attribute
+    {
+        return new Attribute(
+            set: fn($value) => bcrypt($value)
+        );
+    }
 }
