9c6c12f29d6fb89d4a02b2f077f695e2a65fea4a
codesinging <codesinging@gmail.com>
2022-08-05 10:05:56 +0800

    修改数据填充，新增 icp 备案号设置选项

--- database/seeders/SettingOptionSeeder.php
+++ database/seeders/SettingOptionSeeder.php
@@ -29,6 +29,7 @@ class SettingOptionSeeder extends Seeder
             ['group_id' => $this->groupId('site'), 'name' => '网站标题', 'key' => 'site_title', 'type' => SettingTypes::Input->name, 'value' => '专注网络软件开发'],
             ['group_id' => $this->groupId('site'), 'name' => '网站关键词', 'key' => 'site_keywords', 'type' => SettingTypes::Input->name, 'value' => '软件开发,微信小程序开发,微信公众号开发'],
             ['group_id' => $this->groupId('site'), 'name' => '网站描述', 'key' => 'site_description', 'type' => SettingTypes::Textarea->name, 'value' => '品凡网络是一家专注于网络软件开发的创新科技企业'],
+            ['group_id' => $this->groupId('site'), 'name' => 'ICP备案号', 'key' => 'site_icp_beian', 'type' => SettingTypes::Input->name, 'value' => ''],
             ['group_id' => $this->groupId('admin'), 'name' => '是否启用登录验证码', 'key' => 'admin_captcha_enabled', 'type' => SettingTypes::Switch->name, 'value' => false],
         ];
     }
